7dab053bd621fc507cd48df7f0b102a237717f7e
orkunuysal <orkunuysal@gmail.com>
2021-10-29 01:33:54 +0300

    Basket Manager API Added

---
+++ src/Services/BasketManager/BasketManager.API/Entities/Basket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketManager.API.Entities
+{
+    public class Basket
+    {
+        public Basket()
+        {
+
+        }
+
+        public Basket(string userId)
+        {
+            UserId = userId;
+        }
+
+        public String UserId { get; set; }
+        public List<BasketItem> Items { get; set; }
+    }
+}
---
+++ src/Services/BasketManager/BasketManager.API/Service/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using BasketManager.API.Entities;
+using System.Threading.Tasks;
+
+namespace BasketManager.API.Service
+{
+    public interface IBasketService
+    {
+        Task EmptyBasket(string userId);
+        Task<Basket> GetBasket(string userId);
+        Task<Basket> AddToBasket(Basket basket);
+    }
+}
